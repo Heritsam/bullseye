507671209a361fb29124389d4b0860c1890b5129
Ariq Heritsa <ariqhm@gmail.com>
2021-03-03 15:44:14 +0700

    structuring the app

---
+++ bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  bullseye
+//
+//  Created by Ariq Heritsa on 03/03/21.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = 42
+  var score: Int = 0
+  var round: Int = 1
+  
+  func points(sliderValue: Int) -> Int {
+    return 99
+  }
+}
